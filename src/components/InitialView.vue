1d8e5363623bdb9d2df57bf304efd44a3a2b2d43
Jake Applehans <104577130+Applehand@users.noreply.github.com>
2024-09-23 16:53:34 -0500

    pi fix 3

--- src/components/InitialView.vue
+++ src/components/InitialView.vue
@@ -91,8 +91,9 @@ const selectedDomain = ref<string | null>(null);
 const userEmail = ref<string | null>(null);
 
 function startOAuth() {
-  window.location.href = "http://localhost:3000/auth";
+  window.location.href = "https://seowayfinder.applehand.dev:3000/auth";
 }
+
 
 
 function clearProject() {
